b7c028216d4268bd1b473b4703e6a6fdc86428e9
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-05-03 15:01:07 -0700

    added absolute paths for the python scripts

--- yard.sh
+++ yard.sh
@@ -2,11 +2,12 @@
 # Wrapper for Python helper scrips.
 
 subscript=$1
+bindirectory="/Users/`whoami`/bin/python"
 
 # Check for command and run it if it exists.
-if [ -z "$subscript" ] || [ ! -f "python/$subscript.sh" ]; then
+if [ -z "$subscript" ] || [ ! -f "$bindirectory/$subscript.sh" ]; then
   echo 'Unknown command';
   [[ $PS1 ]] && return 2 || exit 2;
 else
-  . python/$subscript.sh;
+  . $bindirectory/$subscript.sh;
 fi
