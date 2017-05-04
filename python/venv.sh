709a5bfa821c3540c6e8499756652d4b5c4a993b
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-05-04 15:02:04 -0700

    added the activate bash function

--- python/venv.sh
+++ python/venv.sh
@@ -1,20 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 # Work with the virtualenv.
 
 op=$2
 
+activate_bash () {
+  if [ -f ".venv/bin/activate" ]; then
+    /bin/bash -c ". .venv/bin/activate; exec /bin/bash -i" ;
+
+    # This wont run until after the activation is complete.
+    echo "Viretual Environment Deactivated";
+
+  else
+    printf \
+      'There was a problem with the activation.\n' \
+      'No activation file found, are you sure this is a python project?\n';
+    exit 1;
+  fi
+}
+
 # Check for command and run it if it exists.
 if [ -z "$op" ]; then
   echo 'venv Expects paramaters (at least 1)';
-  [[ $PS1 ]] && return 2 || exit 2;
+  exit 1;
 fi
 
+# Switch to the virtual environment.
 if [ "$op" == 'activate' ]; then
-  echo "activate";
+  echo "Activating ...";
+  activate_bash
 fi
-
-if [ "$op" == 'deactivate' ]; then
-  echo "deactivate";
-fi
-
-# @TODO Return the actual return code.
