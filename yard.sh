d3f349cd52c8c0e40b8913a5fce8a22e5f0690a2
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-05-04 15:00:59 -0700

    changed to the proper error exit code 1

--- yard.sh
+++ yard.sh
@@ -7,7 +7,7 @@ bindirectory="/Users/`whoami`/bin/python"
 # Check for command and run it if it exists.
 if [ -z "$subscript" ] || [ ! -f "$bindirectory/$subscript.sh" ]; then
   echo 'Unknown command';
-  [[ $PS1 ]] && return 2 || exit 2;
+  exit 1;
 else
   . $bindirectory/$subscript.sh "$@";
 fi
