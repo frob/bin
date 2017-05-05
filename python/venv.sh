fa5732e0ac0dbe16b70779c6c0b2d2e819c793d9
Frank Anderson <frob@249517.no-reply.drupal.org>
2017-05-05 12:22:25 -0700

    added install op

--- python/venv.sh
+++ python/venv.sh
@@ -18,6 +18,47 @@ activate_bash () {
   fi
 }
 
+install () {
+  # If requirements.txt exists, install exactly what is in requirements.txt
+  if [ -f "requirements.txt" ] && [ -f ".venv/bin/activate" ]; then
+    # Install requirements.
+    source .venv/bin/activate;
+    if pip install -r requirements.txt; then
+      echo 'Requirments installed.';
+    else
+      echo 'There was a problem installing requirements.';
+      exit 1;
+    fi
+  elif [ -f "requirements.unlocked.txt" ] && [ -f ".venv/bin/activate" ]; then
+    # Install requirements from unlocked requirements.
+    source .venv/bin/activate;
+    if pip install -r requirements.unlocked.txt; then
+      echo 'Requirments installed. Creating Freeze file: requirements.txt';
+      pip freeze > requirements.txt;
+    else
+      echo 'There was a problem installing requirements.';
+      exit 1;
+    fi
+  else
+    # Exit with an error if not in a venv.
+    if [ ! -f ".venv/bin/activate" ]; then
+      echo "Not an initialized virtual environment.";
+      exit 1;
+    fi
+
+    # Exit without error if no requirements found.
+    if [ ! -f "requirements.txt" ] || [ ! -f "requirements.unlocked.txt" ]; then
+      echo "No requirements found. Are you sure you have a requirements file?";
+    fi
+  fi
+}
+
+# update () {
+#   # Install dependencies from requirements.dev.txt and update requirements.txt
+# pip install -U 'flask>=0.11'
+# https://pip.readthedocs.io/en/stable/reference/pip_install/#pip-install-examples
+# }
+
 # Check for command and run it if it exists.
 if [ -z "$op" ]; then
   echo 'venv Expects paramaters (at least 1)';
@@ -28,4 +69,10 @@ fi
 if [ "$op" == 'activate' ]; then
   echo "Activating ...";
   activate_bash
+fi
+
+if [ "$op" == 'install' ]; then
+  echo "Installing"
+  install
+  exit 0;
 fi
